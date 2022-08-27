350f64bd7d595c41ad93561f30e2390a75998894
Dusan-O <dusan.orescanin@gmail.com>
2022-08-27 20:10:46 +0200

    Tableview

--- GenieAladdin/MyTabView.swift
+++ GenieAladdin/MyTabView.swift
@@ -11,11 +11,25 @@ struct MyTabView: View {
     @State var selection = 0
     var body: some View {
         TabView(selection: $selection) {
-            <#code#>
+            HomePageView()
+                .tabItem {
+                    Text("Accueil")
+                        Image(systemName: "house.fill")
+                    }.tag(0)
+            WishesView()
+                .tabItem {
+                    Text("Réalisations")
+                        Image(systemName: "lamp.desk.fill")
+                    }.tag(1)
+            ContactView()
+                .tabItem {
+                    Text("Contact")
+                        Image(systemName: "globe")
+                    }.tag(2)
         }
     }
 }
-
+            
 struct MyTabView_Previews: PreviewProvider {
     static var previews: some View {
         MyTabView()
