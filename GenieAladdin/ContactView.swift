223f4f98fa26deb50c6ebb46c1e6c87b58f6f489
Dusan-O <dusan.orescanin@gmail.com>
2022-08-27 21:04:09 +0200

    ContactView App version 1.0.0

--- GenieAladdin/ContactView.swift
+++ GenieAladdin/ContactView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct ContactView: View {
+    @State var buttonClicked = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Ou me trouver?")
+                .font(.title)
+                .foregroundColor(.accentColor)
+                .bold()
+            Image("cave_outside")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                .padding()
+            Button("Dans la cave des merveilles") {
+                buttonClicked.toggle()
+            }.buttonStyle(.borderedProminent)
+                .sheet(isPresented: $buttonClicked) {
+                    VStack {
+                        Text("Bien au chaud à l'intérieur de la lampe")
+                            .foregroundColor(.accentColor)
+                            .bold()
+                        Image("dans_lampe")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+            Image("cave_inside")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                .shadow(color: .accentColor, radius: 2, x: 3, y: 3)
+                .padding()
+            Spacer()
+            Text("Attention !!! \nVous ne pouvez prendre que la lampe dans cette cave aux merveilles !")
+                .font(.caption)
+                .bold()
+                .italic()
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+        }
     }
 }
 
