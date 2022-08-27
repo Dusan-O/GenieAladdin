93dc340eac0effae38fb5a19e36e3821bfd0a536
Dusan-O <dusan.orescanin@gmail.com>
2022-08-27 19:59:52 +0200

    Initial Commit

---
+++ GenieAladdin/GenieAladdinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenieAladdinApp.swift
+//  GenieAladdin
+//
+//  Created by Dusan Orescanin on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GenieAladdinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
